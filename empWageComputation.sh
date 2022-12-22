cbe6b709966c40a99b1313be047ea9a5da2e89d0
piyush-shaw <piyushshaw0000@gmail.com>
2022-12-22 13:59:46 +0530

    solving using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,17 +4,19 @@ echo "Welcome to Employee Wage Computation Program"
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
-randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-echo "Employee is full-time present"
+empCheck=$((RANDOM%3));
+case $empCheck in
+$isFullTime)
+echo "Employe is full-time present"
 empHrs=8
-elif [ $isPartTime -eq $randomCheck ]
-then
+;;
+$isPartTime)
 echo "Employee is part-time present"
 empHrs=4
-else
+;;
+*)
 echo "Employee is absent"
 empHrs=0
-fi
+;;
+esac
 salary=$(($empHrs*$empRatePerHr))
